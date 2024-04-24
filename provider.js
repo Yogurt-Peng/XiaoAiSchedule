ead2c5ed56500c476bb63640dca37e2cd4091570
Peng Xinshun <60318979+Yogurt-Peng@users.noreply.github.com>
2024-04-24 16:42:14 +0800

    Update provider.js

--- provider.js
+++ provider.js
@@ -1,9 +1,11 @@
 async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) 
 {
-  var frame=dom.getElementsByName('Frame1')[0].offsetParent
-  .children.mainCenterPanle.children[0].lastElementChild.children[1]
-  .contentDocument.activeElement.childNodes[4].childNodes[1].childNodes[5].childNodes[6].outerHTML
+  var frame = dom.getElementsByName('Frame1')[0].offsetParent
+    .children.mainCenterPanle.children[0].lastElementChild.children[1]
+    .contentDocument.activeElement.childNodes[4].childNodes[1].childNodes[5].childNodes[6].outerHTML
 
-
+ 
+  window. alert("请点击右上角的理论课表后再导入哦!");
+  
   return frame
 }
